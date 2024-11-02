c7f494729ddc980471845c6ceab8da7787a410d5
AndreyGOIT <ae.borga@gmail.com>
2024-11-02 22:47:14 +0200

    refact books.module

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { BooksController } from './books.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksService } from './books.service';
+import { BooksController } from './books.controller';
+import { Book } from './book.entity';
+import { BooksRepository } from './books.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Book])],
   controllers: [BooksController],
-  providers: [BooksService]
+  providers: [BooksService, BooksRepository],
 })
 export class BooksModule {}
