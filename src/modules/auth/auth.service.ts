61b6a0d6e69add4f863add34e58b70315313d550
AndreyGOIT <ae.borga@gmail.com>
2024-11-11 13:01:18 +0200

    organize AuthService

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersRepository } from '../users/users.repository';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userRepository: UsersRepository) {}
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<{ userId: number }> {
+    const user = await this.userRepository.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const isValid = await bcrypt.compare(password, user.passwordHash);
+    if (!isValid) {
+      throw new UnauthorizedException();
+    }
+    return {
+      userId: user.id,
+    };
+  }
+}
