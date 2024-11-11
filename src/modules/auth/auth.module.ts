1ce04bab84ea5b0e4e255501e7b7e84b3e076bf6
AndreyGOIT <ae.borga@gmail.com>
2024-11-11 11:23:40 +0200

    imports in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'secret_key', // Секретный ключ для подписи JWT (должен браться из .env)
+      signOptions: { expiresIn: '60m' }, // Установите желаемый срок действия токена
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
